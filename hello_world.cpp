655ddedd978f69b2a70886e94744ecc9359c8a8a
Artyom Mazurenko <artemmazurenko71@gmail.com>
2024-12-15 15:15:17 +0300

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,13 @@
 #include <string>
 
 int main() {
+    // имя пользоваетля
     std::string name;
+    // запрос имени
     std::cout << "Enter your name:" << std::endl;
+    // ввод имени
     std::cin >> name;
+    // вывод
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
