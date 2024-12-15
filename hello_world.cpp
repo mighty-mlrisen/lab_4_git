a592d16679633d2a674e1486639927262f666dab
Artyom Mazurenko <artemmazurenko71@gmail.com>
2024-12-15 15:45:06 +0300

    change comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    // имя пользоваетля
-    std::string name;
-    // запрос имени
-    std::cout << "Enter your name:" << std::endl;
-    // ввод имени
-    std::cin >> name;
-    // вывод
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+int
+main()
+{
+  // user's name
+  std::string name;
+  // request for name
+  std::cout << "Enter your name:" << std::endl;
+  // input name
+  std::cin >> name;
+  // output
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
